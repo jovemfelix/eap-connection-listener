d38244621333e72685c1f776128744cb173ebd92
Renato Alves Felix <rfelix@redhat.com>
2021-08-25 09:49:49 -0300

    wip trying get user from request or ejb session

--- src/main/java/com/datasource/connection/listener/ClientIdentifierConnectionPreparer.java
+++ src/main/java/com/datasource/connection/listener/ClientIdentifierConnectionPreparer.java
@@ -3,41 +3,47 @@ package com.datasource.connection.listener;
 import org.jboss.jca.adapters.jdbc.spi.listener.ConnectionListener;
 import org.jboss.logging.Logger;
 
-//import javax.annotation.Resource;
-//import javax.ejb.SessionContext;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 /*
 <connection-listener class-name="com.datasource.connection.listener.ClientIdentifierConnectionPreparer"/>
  */
 public class ClientIdentifierConnectionPreparer implements ConnectionListener {
     private static final Logger logger = Logger.getLogger(ClientIdentifierConnectionPreparer.class);
 
-//    @Resource
-//    private SessionContext sessionContext;
+    @Resource
+    private SessionContext sessionContext;
+
+    @Context
+    private HttpServletRequest httpRequest;
 
     private String prepSql = "{ call DBMS_SESSION.SET_IDENTIFIER(?) }";
 
     @Override
     public void initialize(ClassLoader classLoader) {
-        logger.info("[initialize] INFO");
-        logger.debug("[initialize] DEBUG");
-
-        System.out.println("[initialize] classLoader=" + classLoader);
-//        System.out.println("[initialize] sessionContext=" + sessionContext);
+        logger.info("[initialize] classLoader=" + classLoader);
+        logger.info("[initialize] sessionContext=" + sessionContext);
+        logger.info("[initialize] httpRequest=" + httpRequest);
     }
 
     @Override
     public void activated(Connection connection) throws SQLException {
-        System.out.println("[activated] connection=" + connection);
-//        System.out.println("[activated] sessionContext=" + sessionContext);
+        logger.info("[activated] connection=" + connection);
+        logger.info("[activated] sessionContext=" + sessionContext);
+        logger.info("[activated] httpRequest=" + httpRequest);
     }
 
     @Override
     public void passivated(Connection connection) throws SQLException {
-        System.out.println("[passivated] connection=" + connection);
-//        System.out.println("[passivated] sessionContext=" + sessionContext);
+        logger.info("[passivated] connection=" + connection);
+        logger.info("[passivated] sessionContext=" + sessionContext);
+        logger.info("[passivated] httpRequest=" + httpRequest);
     }
 
     protected Connection setClientIdentifier(Connection connection) throws SQLException {
